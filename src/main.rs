141461c2994e27b4db89c81af2be5cde14959523
Yuku Kotani <yukukotani@gmail.com>
2022-07-16 12:16:18 +0900

    review-pr action

--- src/main.rs
+++ src/main.rs
@@ -41,21 +41,20 @@ fn main() {
         } => get_org_members(&org),
     };
 
-    match opt.action.to_lowercase().as_str() {
-        "openpr" => open_pr_command(opt, members),
+    let get_count = match opt.action.to_lowercase().as_str() {
+        "open-pr" => get_open_pr_count,
+        "review-pr" => get_review_pr_count,
         _ => {
             eprintln!("Invalid action: {}", opt.action);
             std::process::exit(1);
         }
-    }
-}
+    };
 
-fn open_pr_command(opt: Opt, members: Vec<String>) {
     let entries = members
         .chunks(10)
         .flat_map(|users| {
             println!("Fetching data of {:?}", users);
-            return get_open_pr_count(users, &opt);
+            return get_count(users, &opt);
         })
         .collect::<Vec<_>>();
 
@@ -74,7 +73,54 @@ fn get_open_pr_count(users: &[String], opt: &Opt) -> Vec<RankingEntry> {
         .enumerate()
         .map(|(i, user)| {
             let query = format!(
-                "author:{} type:pr org:{} {}",
+                "type:pr author:{} org:{} {}",
+                user,
+                opt.org,
+                opt.query.as_ref().unwrap_or(&"".to_string())
+            );
+            return format!(
+                "{}: search(query: \"{}\", type: ISSUE, first: 0) {{ issueCount }}",
+                format!("user_{}", i),
+                query
+            );
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let query = format!(
+        "query {{
+            {}
+        }}",
+        search_queries
+    );
+
+    let response: HashMap<String, IssueCount> = graphql::query(query);
+
+    return response
+        .iter()
+        .map(|(key, issue_count)| {
+            let i = key[5..].parse::<usize>().unwrap();
+            return RankingEntry {
+                name: users[i].to_string(),
+                count: issue_count.issue_count,
+            };
+        })
+        .collect::<Vec<_>>();
+}
+
+fn get_review_pr_count(users: &[String], opt: &Opt) -> Vec<RankingEntry> {
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    struct IssueCount {
+        issue_count: u64,
+    }
+
+    let search_queries = users
+        .iter()
+        .enumerate()
+        .map(|(i, user)| {
+            let query = format!(
+                "type:pr reviewed-by:{0} -author:{0} org:{1} {2}",
                 user,
                 opt.org,
                 opt.query.as_ref().unwrap_or(&"".to_string())
